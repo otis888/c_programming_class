d0c86d11eee5c9d00f71eee2a0beed0ac7f46ae8
otis888 <99777128+otis888@users.noreply.github.com>
2022-02-16 10:19:09 +0800

    Create emoji.c

---
+++ week_1/emoji.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) {
+    printf("Hello my name is john\n");
+    printf("i am 20 years old");
+    printf(" ");
+    printf("i am from DC\n");
+    printf("    ⊂_ヽ\n");
+    printf("     ＼＼\n");
+    printf("      ＼( ͡° ͜ʖ ͡°)\n");
+    printf("        >　⌒ヽ\n");
+    printf("       / 　 へ＼\n");
+    printf("      /　　/　＼＼\n");
+    printf("     ﾚ　ノ　　 ヽ_つ\n");
+    printf("    /　/|\n");
+    printf("   (　(ヽ\n");
+    printf("   |　|、＼\n");
+    printf("   | 丿 ＼ ⌒)\n");
+    printf("   | |　　) /\n");
+    printf("  ノ )　　Lﾉ\n");
+    printf(" (_／\n");
+    return 0;
+}
