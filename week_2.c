4a2a2fbbcf6d9b4ab02788797bfd4b899dc42c4b
otis888 <99777128+otis888@users.noreply.github.com>
2022-02-23 09:53:11 +0800

    Create week_2.c

---
+++ week_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) { 
+    printf("Don't give up and don't give in\n");
+    printf("It's all in the lord's hand\n");
+    printf("I do my very best each day\n");
+    printf("Though some days prove worse than others\n");    
+    printf("I'm everywhere I'm needed\n");
+    printf("No need more crucial than another\n");
+  return 0;
+}
