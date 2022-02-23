19f7f03ad8c558519ede27ec0c84418a821e5bc4
otis888 <99777128+otis888@users.noreply.github.com>
2022-02-23 10:52:23 +0800

    Create veriables.c

---
+++ week_2/veriables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int myweight=88;
+    printf("my weight is %d.\n", myweight);
+
+    double myheight=1.8;
+    printf("my height is %fl.\n", myheight);
+
+    char myName[]="Galbadrakh";
+    printf("my name is %s.\n", myName);
+
+    return 0;
+}
