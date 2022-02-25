2d4d7603ac70c9f9c277d1671c71171aedd31a55
otis888 <99777128+otis888@users.noreply.github.com>
2022-02-25 16:12:41 +0800

    Create math.c

---
+++ week_2/math.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //add
+    int a=1+2;
+    printf("%d \n", a);
+
+    //substract
+    int b=100-2;
+    printf("%d \n", b);
+
+    //multiply
+    int c=5 * 5;
+    printf("%d \n", c);
+
+    //divide
+    int d=46/2;
+    printf("%d \n", d);
+    
+    return 0;
+}
