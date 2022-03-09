d257b24c14a0f45cb0257c4cc7a828511d24fc74
otis888 <99777128+otis888@users.noreply.github.com>
2022-03-09 10:32:21 +0800

    Create if.c

---
+++ week_4/if.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+    int too = 2;
+    if (too == 3)
+    {
+        printf("guraw\n");
+    }
+    if (too < 3)
+    {
+        printf("gurawaas baga\n");
+    }
+    if (too >= 3)
+    {
+        printf("gurawaas ih buya tentsuu\n");
+    }
+    if (too <= 3)
+    {
+        printf("gurawaas baga buyu tentsuu\n");
+    }
+
+    return 0;
+}
