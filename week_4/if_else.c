e4960c9a9ed3bb156553724917115982df28274d
otis888 <99777128+otis888@users.noreply.github.com>
2022-03-09 10:39:02 +0800

    Create if_else.c

---
+++ week_4/if_else.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void)
+{
+    int too = 2;
+    if (too == 3)
+    {
+        printf("guraw\n");
+    }
+    else if (too < 3)
+    {
+        printf("gurawaas baga\n");
+    }
+    else if (too > 3)
+    {
+        printf("gurawaas ih\n");
+    }
+    char useg = 'a';
+    if (useg == 'x')
+    {
+        printf("x\n");
+    }
+    else
+    {
+        printf("x bish\n");
+    }
+
+    return 0;
+}
