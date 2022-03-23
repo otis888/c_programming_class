18bddb696d6a4a9ad6728ffbebaf63a14a90ffb3
otis888 <99777128+otis888@users.noreply.github.com>
2022-03-23 08:47:14 +0800

    Update functions.c

--- week_6/functions.c
+++ week_6/functions.c
@@ -13,3 +13,17 @@ int main(void)
 
     return 0;
 }
+/* #include <stdio.h>
+#include <math.h>
+
+double areaCircle(double radius)
+{
+    return radius * radius * M_PI;
+}
+int main(void)
+{
+    double area = areaCircle(2);
+    printf("%lf\n", area);
+
+    return 0;
+} */
