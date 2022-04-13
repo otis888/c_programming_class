8205d619c702002b2ad4ee2e9cc0cb736795f65c
otis888 <99777128+otis888@users.noreply.github.com>
2022-04-13 10:45:55 +0800

    Create switch.c

---
+++ week_9/switch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char letter = 'a';
+    if (letter == 'a')
+    {
+        printf("apple\n");
+    }
+    else if (letter == 'b')
+    {
+        printf("banana\n");
+    }
+    else if (letter == 'c')
+    {
+        printf("cat\n");
+    }
+    else if (letter == 'd')
+    {
+        printf("other\n");
+    }
+
+    return 0;
+}
